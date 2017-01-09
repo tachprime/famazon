a8a525eb0de4dab4c018236882b985cf67733c99
Alex Rivera <alexis_rivera_31@hotmail.com>
2017-01-08 21:28:05 -0500

    connected to database

--- famazonCustomer.js
+++ famazonCustomer.js
@@ -5,8 +5,12 @@ const mysql = require('mysql');
 var connection = mysql.createConnection({
 	host: 'localhost',
 	port: 3306,
-	user: root,
+	user: 'root',
 	password: userPass.password,
 	databse: 'Bamazon'
 });
 
+connection.connect((err) => {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+});
